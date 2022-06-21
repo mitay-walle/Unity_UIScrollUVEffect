38658f7c04a14e09cb16878d32eb3f129ab79553
mitay-walle <mitay-walle91@mail.ru>
2022-06-21 22:25:05 +0300

    Update UVScrollEffect.cs

--- UVScrollEffect.cs
+++ UVScrollEffect.cs
@@ -3,41 +3,57 @@ using UnityEngine.UI;
 
 public class UVScrollEffect : BaseMeshEffect
 {
-    [SerializeField] private Vector2 scroll = new Vector2(1, 1);
+      [SerializeField] private Vector2 _scroll = new Vector2(1, 1);
+        [SerializeField] private bool _playEditor;
+        private Vector2 _scrollDelta;
 
-    private Image _image;
-    private Vector2 scrollDelta;
-
-    protected override void Start()
-    {
-        _image = GetComponent<Image>();
-    }
-
-    public override void ModifyMesh(VertexHelper vh)
-    {
-        if (!IsActive()) return;
-        if (!_image) return;
-
-        int vertCount = vh.currentVertCount;
-
-        scrollDelta = scroll * Time.time;
-
-        UIVertex vert = new UIVertex();
-
-        for (var i = 0; i < vertCount; i++)
+        protected override void OnEnable()
         {
-            vh.PopulateUIVertex(ref vert, i);
+            base.OnEnable();
+            EditorApplication.update -= Repaint;
+            EditorApplication.update += Repaint;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            EditorApplication.update -= Repaint;
+        }
+
+        public override void ModifyMesh(VertexHelper vh)
+        {
+            if (!IsActive()) return;
+            if (!_playEditor || Application.isPlaying) return;
+
+            int vertCount = vh.currentVertCount;
+
+            _scrollDelta = _scroll * Time.time;
+
+            UIVertex vert = new UIVertex();
+
+            for (var i = 0; i < vertCount; i++)
+            {
+                vh.PopulateUIVertex(ref vert, i);
 #if UNITY_2020_2_OR_NEWER
-            vert.uv0 += (Vector4)scrollDelta;
+                vert.uv0 += (Vector4)_scrollDelta;
 #else
             vert.uv0 += scrollDelta;
 #endif
-            vh.SetUIVertex(vert, i);
+                vh.SetUIVertex(vert, i);
+            }
         }
-    }
 
-    private void Update()
-    {
-        _image.SetVerticesDirty();
-    }
+        private void Update()
+        {
+            graphic.SetVerticesDirty();
+        }
+
+#if UNITY_EDITOR
+        private void Repaint()
+        {
+            if (!_playEditor || Application.isPlaying) return;
+            EditorApplication.QueuePlayerLoopUpdate();
+            graphic.SetVerticesDirty();
+        }
+#endif
 }
